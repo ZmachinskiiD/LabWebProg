af118f35b214d81828f73f1bc6a19481458eff05
ZmachinskiiD <zadira420@gmail.com>
2022-02-11 15:08:35 +0200

    Stage 11

--- code/index.php
+++ code/index.php
@@ -8,12 +8,24 @@ $order .=" and a small diet coke";
 echo "\nYour order is: $very_bad_unclear_name.";
 //Пункт 2
 echo "<br>";
+echo "Пункт 2"."<br>";
+echo "<br>";
 $Number = 33;
+$TwentyOne=21;
 echo $Number."<br>";
 echo "\n" ;;
 $SDRT = 12.34;
 echo $SDRT. "<br>";
-echo "12". "<br>";
+echo ($Number-$TwentyOne). "<br>";
 $last_Month = 1187.23;
 $this_Month = 1089.98;
 echo ($last_Month-$this_Month)."<br>";
+//Stage 3(11)///////////////////////////////////////
+echo "<br>";
+echo "Пункт 3(11)"."<br>";
+$num_languages = 4;
+$months = 11;
+$days = $months * 16;
+$days_per_language = $days;
+echo $days_per_language."<br>";
+
