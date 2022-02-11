b184e536a78d0905e058be2a42d8a43446ff0cbb
ZmachinskiiD <zadira420@gmail.com>
2022-02-11 14:49:46 +0200

    WORK on 2 stage
    sample text

--- code/index.php
+++ code/index.php
@@ -6,3 +6,12 @@ $order = &$very_bad_unclear_name;
 $order .=" and a small diet coke";
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
+//Пункт 2
+echo "<br>";
+$Number = 33;
+$SecondNumber = 129;
+echo $Number."<br>";
+echo "\n" ;
+echo $SecondNumber."<br>";
+$SDRT = 12.34;
+echo $SDRT. "<br>";
