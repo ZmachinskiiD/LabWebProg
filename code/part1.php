a22c22e07a1e28a50b3f5f47e1be9ac67c1b5c30
ZmachinskiiD <zadira420@gmail.com>
2022-02-24 20:02:30 +0200

    Update part1.php
    1B

--- code/part1.php
+++ code/part1.php
@@ -15,3 +15,19 @@ foreach ($M_str[0] as $m1)
 {
     echo " " . $m1;
 }
+////////////////////TASK B/////////////////////////////
+echo "<br>". "ПУНКТ 1B". "<br>";
+$str = "a1b2c3";
+echo "Начальная строка". "<br>";
+echo $str . "<br>";
+$cubify = preg_replace_callback
+(
+    "/[0-9]+/",
+    function (array $m2)
+    {
+        return ((int)($m2[0]) ** 3)."";
+    },
+    $str
+);
+echo  "Изменённая строка: ". "<br>" ;
+echo  $cubify;
