a0ec53f5312e18c209864ecc06c4665c454247b7
ZmachinskiiD <zadira420@gmail.com>
2022-02-24 19:53:57 +0200

    Update part1.php
    1A

--- code/part1.php
+++ code/part1.php
@@ -1,2 +1,17 @@
 <?php
-$str = "ahb acb aeb aeeb adcb axeb";
+////////////////////TASK A//////////////////////////////
+echo "ПУНКТ 1А";
+echo "<br>" ;
+echo "Все строки";
+echo "<br>" ;
+$str = " aqqwb adcb ahb acb aeb aeeb adcb axeb yabv aeebr aytb assb ghty oelro ";
+$M_str = [];
+echo $str;
+echo "<br>" ;
+preg_match_all("/a..b/", $str, $M_str);
+echo  "Строки удовлетворяющие критериям:";
+echo "<br>" ;
+foreach ($M_str[0] as $m1)
+{
+    echo " " . $m1;
+}
