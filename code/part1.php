8f6d0ef4f9269bc872a868ab41e2e9cea00824c5
ZmachinskiiD <zadira420@gmail.com>
2022-02-25 22:44:01 +0200

    Update part1.php
    did 2a

--- code/part1.php
+++ code/part1.php
@@ -31,3 +31,47 @@ $cubify = preg_replace_callback
 );
 echo  "Изменённая строка: ". "<br>" ;
 echo  $cubify;
+
+echo "<br><br>";
+
+
+// task 2(a)
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>reg exp and file work</title>
+    </head>
+    <body>
+    <form method="POST">
+        <label>
+<textarea name="textToParse"
+          placeholder="Input your stuff"
+          rows="20" cols="50"></textarea>
+        </label>
+        <input type="submit" name="getWordsAndSymbols" value="Сount stuff ">
+    </form>
+    </body>
+    </html>
+
+<?php
+if ($_POST['getWordsAndSymbols'])
+{
+    if ($_POST['textToParse'])
+    {
+        $_SESSION['textInfo'] = str_word_count($_POST['textToParse'],
+                0) . ' Words<br>';
+
+        $_SESSION['textInfo'] .= strlen($_POST['textToParse']) . ' Symbols<br>';
+
+    } else
+    {
+
+        $_SESSION['textInfo'] = 'U didnt put anything u stupid';
+    }
+}
+echo $_SESSION['textInfo']; 
