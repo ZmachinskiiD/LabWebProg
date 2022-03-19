3c473c9cfec24bbef60da7ec8240276fb9540a43
ZmachinskiiD <zadira420@gmail.com>
2022-03-19 14:34:28 +0200

    Update billboard.php
    Rewritten billboard for 4lab

--- code/billboard.php
+++ code/billboard.php
@@ -1,97 +1,111 @@
 <?php
-session_start()
+require "../vendor/autoload.php";
+$client = new Google_Client();
+$client->setApplicationName('Google Sheets API PHP Quickstart');
+$client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
+$client->setAuthConfig('../credentials.json');
+$client->setAccessType('offline');
+$client->setPrompt('select_account consent');
+$service = new Google_Service_Sheets($client);
+$spreadsheetId = '13ixJlPRuhkFhySTPfW5Suu65t5iHVZYfPrTC0-y42uE';
+$range = 'AdData!A1:D';
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
+var_dump($values);
 ?>
-
-    <!doctype html>
-    <html lang="ru">
-    <head>
-        <meta charset="UTF-8">
-        <title>Биллборд</title>
-    </head>
-    <body>
-    <form method="post">
-        <label>
-            e-mail<br>
-            <input type="email" name="email"><br>
-            Category<br>
-            <select name="type" required>
-                <option>Pets</option>
-                <option>Meetings</option>
-                <option>Shoes</option>
-                <option>Houses</option>
-            </select><br>
-            Headline<br>
-            <input type="text" name="headline"><br>
-            Text<br>
-            <textarea cols="50" rows="5" name="text"></textarea>
-            <br><br>
-            <input type="submit" name="submit" value="add"><br><br>
-        </label>
-    </form>
-    </body>
-    </html>
-
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Billboard</title>
+</head>
+<body>
+<form action="/saveinfo.php" method="GET">
+    <label>
+        Your e-mail:<br>
+        <input type="email" name="emailNew"
+               value="<?php
+               if (isset($_SESSION["userEmail"]))
+                   echo $_SESSION["userEmail"];
+               else
+                   echo ""; ?>"
+               size="30" maxlength="30" required><br>
+        Category:<br>
+        <select name="categoryNew" required>
+            <option>Pets</option>
+            <option>Meetings</option>
+            <option>Shoes</option>
+            <option>Houses</option>
+        </select><br>
+        Heading:<br>
+        <input type="text" name="headingNew" value="" size="30" maxlength="30" required><br>
+        Text:<br>
+        <textarea cols="50" rows="10" name="textNew" placeholder="Enter text of your ad" required></textarea>
+        <br><br>
+        <input type="submit" name="PostNew" value="POST"> <input type="reset" value="ERASE"><br><br>
+    </label>
+</form>
+<form action="/save_ad.php" method="GET">
+    Choose category to show:
+    <label>
+        <select name="categorySelect" required>
+            <option>Pets</option>
+            <option>Meetings</option>
+            <option>Shoes</option>
+            <option>Houses</option>
+        </select>
+    </label><br>
+    <input type="submit" name="start" value="SHOW">
+</form>
+</body>
+</html>
+<br>
+<h1> Ads </h1>
+<br>
 <?php
-
-if (!empty($_POST["email"]) and !empty($_POST["type"]) and !empty($_POST["headline"]) and !empty($_POST["text"])) {
-    AddFile($_POST["email"], $_POST["type"], $_POST["headline"], $_POST["text"]);
-}
-
-$path = "./type";
-$d = dir($path);
-$category = [];
-if ($d) {
-    while (false !== ($name = $d->read())) {
-        if ($name === '.' || $name === '..') continue;
-        $FullName = $path . "/" . $name;
-        $category[] = $name;
+$categories = scandir(__DIR__."/categories");
+$ads = [];
+foreach($categories as $category){
+    if ($category != "." && $category != ".."){
+        $ads[$category] = scandir("categories/$category");
     }
-    $d->close();
-}
-
-echo nl2br("\n");
-echo '<table border="1" width="100%">';
-echo '<tr bgcolor="#faebd7">';
-echo '<td width="20%">' . "Category" . '</td>';
-echo '<td width="20%">' . "e-mail" . '</td>';
-echo '<td width="20%">' . "Headline" . '</td>';
-echo '<td width="40%">' . "Info" . '</td>';
-echo '</tr>';
-
-foreach ($category as $SPath) {
-    foreach (FilesFind('./type/' . $SPath) as $file) {
-        echo '<tr>';
-        $fileInfo = file('./type/' . $SPath . "/" . $file);
-        echo '<td>' . $SPath . '</td>';
-        echo '<td>' . $fileInfo[0] . '</td>';
-        echo '<td>' . $fileInfo[1] . '</td>';
-        echo '<td>';
-        for ($i = 2; $i < sizeof($fileInfo); $i++) {
-            echo nl2br($fileInfo[$i]);
+};
+echo "<table border=1><caption>Ads</caption>";
+foreach($ads as $category => $arr){
+    echo "<tr><th colspan=3 align=center>" . ucfirst($category) . "</th></tr>";
+    foreach($arr as $ad){
+        if ($ad != "." && $ad != ".."){
+            $file = fopen("categories/$category/$ad", "r");
+            $email = fgets($file);
+            $headline = fgets($file);
+            $Text = fgets($file);
+            echo "<tr><td>$email</td><td>$headline</td><td>$Text</td></tr>";
         }
-        echo '</td>';
-        echo '</tr>';
     }
 }
-echo '</table>';
+echo "</table>";
+?>
+<body>
+<form>
+    <label>
+        <h1>Ads from GoogleSheets</h1>
+        <table border=1>
+            <tr><th>Category</th><th>Email</th><th>Header</th><th>Text</th></tr>
+            <tbody>
 
-function FilesFind(string $path): array
-{
-    $d = dir($path);
-    $files = array();
-    if ($d) {
-        while (false !== ($name = $d->read())) {
-            if ($name === '.' || $name === '..') continue;
-            $FullName = $path . "/" . $name;
-            if (is_file($FullName)) $files[] = $name;
-        }
-        $d->close();
-    }
-    return $files;
-}
-
-function AddFile(string $email, $category, $headline, $text)
-{
-    $file = fopen("./type/" . $category . "/" . $headline . ".txt", "w");
-    fwrite($file, $email . "\n" . $headline . "\n" . $text);
-}
+            <?php
+            foreach($values as $adData){
+                echo "<tr>" . "<td>" . ucfirst($adData[0]) . "</td>";
+                echo "<td>" . $adData[1] . "</td>";
+                echo "<td>" . $adData[2] . "</td>";
+                echo "<td>" . $adData[3] . "</td>" . "</tr>";
+            }
+            ?>
+            </tbody>
+        </table>
+    </label>
+</form>
+</body>}
